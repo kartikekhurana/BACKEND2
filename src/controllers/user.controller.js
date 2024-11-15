8244c5fb2351ef76135f710c2105d0cd28706d04
kartikekhurana <kartikekhurana@gmail.com>
2024-11-15 14:22:48 +0530

    added get watchHistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -379,6 +379,57 @@ return res
 )
 })
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+const user = await User.aggregate([
+    {
+        $match:{
+           _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+    },
+    {
+        $lookup:{
+from:"videos",
+localField:"watchHistory",
+foreignField:"_id",
+as:"watchHistory",
+pipeline:[
+    {
+        $lookup:{
+            from:"users",
+            localField:"owner",
+            foreignField:"_id",
+            as:"owner",
+            pipeline:[
+                {
+                    $project:{
+                      fullname :1,
+                      username:1,
+                      avatar:1,  
+                    }
+                }
+            ]
+        }
+    },
+    {
+$addFields:{
+    owner:{
+        $first: "$owner"
+    }
+}
+}
+]
+        }
+    }
+])
+
+
+return res.
+status(200)
+.json(
+    new ApiResponse(
+        200,user[0],"this is your watch history"    )
+)
+})
 
 
 export {registerUser,
@@ -390,6 +441,7 @@ export {registerUser,
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 
 };
